501a6738664f2c4d93b865b8dee50acd8b209579
Eduardo Pacheco <du@kanema.combr>
2016-01-16 20:46:48 -0200

    - Incluir sensores de luminosidade. - Incluir lógica para busca de luz. - Incluir sleep.

--- RTP800.ino
+++ RTP800.ino
@@ -2,13 +2,19 @@
  
 Servo servo;
 
+const int D_STOP = 0;
+const int D_LEFT = 1;
+const int D_RIGHT = 2;
+const int D_FRONT = 3;
+int currentDirection = D_STOP;
+
 const int motorRight = 5;
 const int motorLeft = 6;
 const int motorDirRight = 7;
 const int motorDirLeft = 8;
 
 const int motorMinSpeed = 160;
-const int motorMaxSpeed = 255;
+const int motorMaxSpeed = 200;
 const float motorMaxVoltage = 6.0;
 
 const int echoPinFrontRight = 13;
@@ -20,16 +26,39 @@ const int trigPinFrontLeft = 10;
 const int echoPinBack = 9;
 const int trigPinBack = 4;
 
-int obstacleDistance = 50;
-int directionChangeTime = 1000;
-int stopTime = 500;
-int runTime = 0;
+bool frontRightObstacle;
+bool frontLeftObstacle;
+bool backObstacle;
+bool frontObstacle;
 
-bool stopped = true;
+int sunDelay = 10 * 1000 * 1;
+int lightSearchTime = 100;
+int obstacleDistance = 60;
+int directionChangeTime = 100;
+int stopTime = 50;
+int runTime = 50;
 
 float vPow = 5.0;
 float r1 = 100000.0;
 float r2 = 10000.0;
+
+const int NO_LIGHT = 0;
+const int LEFT_LIGHT = 1;
+const int RIGHT_LIGHT = 2;
+const int FRONT_LIGHT = 3;
+const int GOOD_LIGHT = 4;
+
+const int LDR_MIN_DIFF = 30;
+const int LDR_MIN_LUX = 800;
+const int LDR_REQUIRED_LUX = 300;
+
+int ldr = NO_LIGHT;
+
+int ldrRPin = A1;
+int ldrRValue = 0;
+
+int ldrLPin = A2;
+int ldrLValue = 0;
 
 // ------------------------------------------------
 
@@ -90,35 +119,67 @@ void motors(int motor, bool on = true, bool front = true, int speed = motorMaxSp
   digitalWrite((motor == motorLeft) ? motorDirLeft : motorDirRight, (front) ? HIGH : LOW);
 }
 
-void run(){
+void checkDirection(int direction){
+  bool isAnotherDirection = direction != currentDirection;
+  if (isAnotherDirection) {
+    if (direction != D_STOP) {
+      stop();
+    }
+    currentDirection = direction;
+  }
+}
+
+void run(int time){
+  checkDirection(D_FRONT);
   motors(motorLeft);
   motors(motorRight);
-  delay(runTime);
+  delay(time);
+}
+
+void run()
+{
+  run(runTime);
+}
+
+void stop(int time)
+{
+  checkDirection(D_STOP);
+  motors(motorLeft, false, true, 0);
+  motors(motorRight, false, true, 0);
+  delay(time);
 }
 
 void stop()
 {
-  motors(motorLeft, false, true, 0);
-  motors(motorRight, false, true, 0);
-  delay(stopTime);
+  stop(stopTime);
+}
+
+void turnLeft(int time)
+{
+  checkDirection(D_LEFT);
+  
+  motors(motorLeft, true, false);
+  motors(motorRight, false, false, 0);
+  delay(time);
 }
 
 void turnLeft()
 {
-  stop();
+  turnLeft(directionChangeTime);
+}
+
+void turnRight(int time)
+{
+  checkDirection(D_RIGHT);
   
-  motors(motorLeft, true, false);
-  motors(motorRight, false, false, 0);
-  delay(directionChangeTime);
+  motors(motorRight, true, false);
+  motors(motorLeft, false, false, 0);
+  delay(time);
 }
 
 void turnRight()
 {
-  stop();
-  
-  motors(motorRight, true, false);
-  motors(motorLeft, false, false, 0);
-  delay(directionChangeTime);
+  turnRight(directionChangeTime);
 }
 
 bool hasFrontRightObstacle()
@@ -136,32 +197,107 @@ bool hasBackObstacle()
   return getDistance(echoPinBack, trigPinBack) < obstacleDistance;
 }
 
-void loop(){
-  
-  bool frontRightObstacle = hasFrontRightObstacle();
-  bool frontLeftObstacle = hasFrontLeftObstacle();
-  bool backObstacle = hasBackObstacle();
-  bool frontObstacle = frontRightObstacle || frontLeftObstacle;
+int getLight()
+{
+  ldrRValue = analogRead(ldrRPin);
+  ldrLValue = analogRead(ldrLPin);
+  int ldrRLDiffValue = abs(ldrRValue - ldrLValue);
+  bool hasDiff = ldrRLDiffValue > LDR_MIN_DIFF;
+  bool hasLight = (ldrRValue < LDR_MIN_LUX) && (ldrLValue < LDR_MIN_LUX);
+  bool hasGoodLight = (ldrRValue < LDR_REQUIRED_LUX) && (ldrLValue < LDR_REQUIRED_LUX);
 
+  if (hasGoodLight) {
+    return GOOD_LIGHT;
+  }
+
+  if ( ! hasLight) {
+    return NO_LIGHT;
+  }
+
+  if (hasDiff) {
+    if (ldrRValue < ldrLValue) {
+      return RIGHT_LIGHT;
+    } else {
+      return LEFT_LIGHT;
+    }
+  }
+  
+  return FRONT_LIGHT;
+}
+
+void walk()
+{ 
   if (backObstacle && frontObstacle) {
     stop();
-    Serial.println("no way punk");
   }
   else if (backObstacle) {
     run();
-    Serial.println("hasBackObstacle");
   }
   else if (frontRightObstacle) {
-    turnRight();
-    Serial.println("hasFrontRightObstacle");
+    turnLeft();
   }
   else if (frontLeftObstacle) {
-    turnLeft();
-    Serial.println("hasFrontLeftObstacle");
+    turnRight();
   }
   else {
     run();
-    Serial.println("run");
   }
+}
+
+void sunbath() {
+  Serial.println("sunbath ^_^");
+  delay(sunDelay);
+  Serial.println("back to work. :(");
+}
+
+int cicles = 0;
+int sleepCicle = 100;
+int sleepTime = 10 * 1000 * 1;
+
+void goToSleep()
+{
+  Serial.println("sleep time zZzZ");
+  delay(sleepTime);
+  cicles = 0;
+}
+
+bool checkSleep() {
+  bool needSleep = cicles >= sleepCicle;
+  
+  if (needSleep) {
+    goToSleep();
+  }
+  
+  cicles++;
+}
+
+void loop(){
+
+  checkSleep();
+  
+  frontRightObstacle = hasFrontRightObstacle();
+  frontLeftObstacle = hasFrontLeftObstacle();
+  backObstacle = hasBackObstacle();
+  frontObstacle = frontRightObstacle || frontLeftObstacle;
+  
+  ldr = getLight();
+
+  if (ldr == GOOD_LIGHT) {
+    sunbath();
+  }
+  
+  if (ldr == LEFT_LIGHT && ! frontLeftObstacle) {
+    turnLeft(lightSearchTime);
+  }
+  if (ldr == RIGHT_LIGHT && ! frontRightObstacle) {
+    turnRight(lightSearchTime);
+  }
+  if (ldr == FRONT_LIGHT && ! frontObstacle) {
+    run(lightSearchTime);
+  }
+  else {
+    walk();
+  }
+  
 }
 
